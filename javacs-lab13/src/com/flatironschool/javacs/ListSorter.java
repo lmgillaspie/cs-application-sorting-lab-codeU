df65abfba10f18ee1038ae74a4c2115f739c4859
Lindsey M. Gillaspie <lindsey.gillaspie@gmail.com>
2016-08-06 23:31:58 -0400

    Finito.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Edited by Lindsey Gillaspie 
  */
 package com.flatironschool.javacs;
 
@@ -64,20 +64,48 @@ public class ListSorter<T> {
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+		int size = list.size();
+		if(size <= 1) {
+			return list;
+		}
+		
+		// split the elements into 2 lists
+		List<T> one = mergeSort(new LinkedList<T>(list.subList(0, size/2)), comparator);
+		List<T> two = mergeSort(new LinkedList<T>(list.subList(size/2, size)), comparator);
+
+		return merge(one, two, comparator);
 	}
 
 	/**
-	 * Sorts a list using a Comparator object.
-	 * 
-	 * @param list
-	 * @param comparator
-	 * @return
-	 */
-	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-	}
+ 	 * A single sorted list is created from merging two other lists.
+  	 * 
+       	 */
+       	private List<T> merge(List<T> first, List<T> second, Comparator<T> comparator) {
+       		List<T> result = new LinkedList<T>();
+       		int total_size = first.size() + second.size();
+       		
+		for (int i=0; i < total_size; i++) {
+       			List<T> smaller_first = chooseSmallerFirst(first, second, comparator);
+       			result.add(smaller_first.remove(0));
+       		}
+       		return result;
+       	}
 
+	/**
+	 * The list with smaller first element is chosen, according to `comparator`.
+	 * 
+	 */
+	private List<T> chooseSmallerFirst(List<T> first, List<T> second, Comparator<T> comparator) {
+		if (first.size() == 0) { return second;}
+		if (second.size() == 0) { return first;}
+		
+		int result = comparator.compare(first.get(0), second.get(0));
+		
+		if (result < 0) { return first;}
+		if (result > 0) { return second;}
+		
+		return first;
+	}
 	
 	/**
 	 * Returns the largest `k` elements in `list` in ascending order.
@@ -89,8 +117,42 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        // Completed this
+		PriorityQueue<T> heap = new PriorityQueue<T>(list.size(), comparator);
+		for (T element: list) {
+			if (heap.size() < k) {
+				heap.offer(element);
+				continue;
+			}
+			int cmp = comparator.compare(element, heap.peek());
+		
+			if (cmp > 0) {
+				heap.poll();
+				heap.offer(element);
+			}
+		}
+		
+		List<T> result = new ArrayList<T>();
+		while (!heap.isEmpty()) {
+			result.add(heap.poll());
+		}
+		
+		return result;
+	}
+
+	/**
+	 * Comparator object is used to sort a list.
+	 * 
+	 */
+	public void heapSort(List<T> list, Comparator<T> comparator) {
+	    	PriorityQueue<T> heap = new PriorityQueue<T>(list.size(), comparator);
+	      	heap.addAll(list);
+	      	
+		list.clear();
+	      	
+		while (!heap.isEmpty()) {
+	      		list.add(heap.poll());
+	      	}
 	}
 
 	
